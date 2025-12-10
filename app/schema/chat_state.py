70c1eb8fa6ef2b85bbf90f653025e0c9920f6f6d
pradanaadn <pradanaadn21@gmail.com>
2025-12-10 16:59:59 +0700

    feat: enhance ChatState and ChatService with stock checking functionality

--- app/schema/chat_state.py
+++ app/schema/chat_state.py
@@ -6,7 +6,7 @@ class ChatState(BaseModel):
     telephone_number: str = Field(..., description="Customer's telephone number")
     customer_info: CustomerSchema | None = Field(default_factory=CustomerSchema, description="Customer information")
     user_chat: str = Field(..., description="User's chat message")
-    agent_response: str | None = Field(None, description="Agent's response message")
+    agent_response: str | dict | None = Field(None, description="Agent's response message")
     
     def is_known_customer(self) -> bool:
         return self.customer_info.name is not None
--- app/service/chat_service.py
+++ app/service/chat_service.py
@@ -1,11 +1,40 @@
 from langchain.chat_models import BaseChatModel
 from langgraph.graph import StateGraph, END, START
 # from langgraph.checkpoint.memory import InMemorySaver  
+from langchain.tools import tool
+from langchain.agents import create_agent
+
 from langchain.messages import HumanMessage
 from sqlalchemy.orm import Session
 from app.schema.chat_state import ChatState
 from app.schema.customer import CustomerSchema
 from app.repository.find_customer import find_customer_by_telephone, create_customer
+from app.repository.stock_check import check_stock
+
+
+def create_check_stock_tool(session: Session):
+    @tool
+    def check_stock_tool(product_name: str) -> str:
+        """A tool to find list of stock of product
+
+        Args:
+            product_name (str): Query for product name
+
+        Returns:
+            str: Response, it might contain the list of product or not
+        """
+        stocks = check_stock(session, product_name)
+        list_of_stocks = []
+        if stocks:
+            for stock in stocks:
+                list_of_stocks.append(f"{stock.id}-{stock.product.name}-{stock.quantity}")
+            stocks_str = "\n".join(list_of_stocks)
+            response = f"Stok untuk produk {product_name} adalah sebagai berikut:\n {stocks_str}"
+        else:
+            response = f"Maaf, produk {product_name} tidak ditemukan dalam inventaris kami."
+        return response
+    return check_stock_tool
+
 
 class ChatService:
     def __init__(self, chat_model: BaseChatModel, session: Session):
@@ -45,6 +74,8 @@ class ChatService:
             )
         return state
     
+    
+    
     def should_greet(self, state: ChatState) -> str:
         if not state.user_chat or state.user_chat.strip() == "":
             
@@ -63,8 +94,12 @@ class ChatService:
         return state
     
     def chat(self, state: ChatState) -> ChatState:
-        updated_state = self.chat_model.invoke([HumanMessage(content=state.user_chat)])
-        state.agent_response = updated_state.content
+        tools = [create_check_stock_tool(self.session)]
+        agent = create_agent(self.chat_model, tools, system_prompt="Anda adalah asisten layanan pelanggan yang ramah dan membantu. Anda dapat membantu pelanggan dengan pertanyaan mereka tentang produk dan layanan kami. Jika pelanggan menanyakan tentang ketersediaan produk, gunakan alat pemeriksaan stok untuk memberikan informasi yang akurat.")
+        updated_state = agent.invoke(    {"messages": [{"role": "user", "content": state.user_chat}]},
+)
+        print(updated_state)
+        state.agent_response = updated_state
         return state
     
     def parse_customer_info(self, state: ChatState) -> CustomerSchema | None:
